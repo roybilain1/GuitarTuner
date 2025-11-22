d01033dcb353fb0e20097bddcf3d3174288f758c
roybilain1 <52330900@students.liu.edu.lb>
2025-11-22 16:16:09 +0200

    added the other buttons to their right places

--- lib/tuning.dart
+++ lib/tuning.dart
@@ -57,6 +57,156 @@ class _TunningState extends State<Tuning> {
               ),
             ),
           ),
+
+          // A string button
+          Positioned(
+            left: 70,
+            top: 460,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                backgroundColor: Colors.white,
+                padding: EdgeInsets.all(0),
+                elevation: 6,
+              ),
+              onPressed: () {
+                // TODO: Add action for E string
+              },
+              child: Container(
+                width: 60,
+                height: 60,
+                alignment: Alignment.center,
+                child: Text(
+                  'A',
+                  style: TextStyle(
+                    color: Color(0xFF800020),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // D string button
+          Positioned(
+            left: 90,
+            top: 360,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                backgroundColor: Colors.white,
+                padding: EdgeInsets.all(0),
+                elevation: 6,
+              ),
+              onPressed: () {
+                // TODO: Add action for E string
+              },
+              child: Container(
+                width: 60,
+                height: 60,
+                alignment: Alignment.center,
+                child: Text(
+                  'D',
+                  style: TextStyle(
+                    color: Color(0xFF800020),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // G string button
+          Positioned(
+            left: 110,
+            top: 260,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                backgroundColor: Colors.white,
+                padding: EdgeInsets.all(0),
+                elevation: 6,
+              ),
+              onPressed: () {
+                // TODO: Add action for E string
+              },
+              child: Container(
+                width: 60,
+                height: 60,
+                alignment: Alignment.center,
+                child: Text(
+                  'G',
+                  style: TextStyle(
+                    color: Color(0xFF800020),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // B string button
+          Positioned(
+            left: 130,
+            top: 160,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                backgroundColor: Colors.white,
+                padding: EdgeInsets.all(0),
+                elevation: 6,
+              ),
+              onPressed: () {
+                // TODO: Add action for E string
+              },
+              child: Container(
+                width: 60,
+                height: 60,
+                alignment: Alignment.center,
+                child: Text(
+                  'B',
+                  style: TextStyle(
+                    color: Color(0xFF800020),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // High E string button// Low E string button
+          Positioned(
+            left: 150,
+            top: 60,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                backgroundColor: Colors.white,
+                padding: EdgeInsets.all(0),
+                elevation: 6,
+              ),
+              onPressed: () {
+                // TODO: Add action for E string
+              },
+              child: Container(
+                width: 60,
+                height: 60,
+                alignment: Alignment.center,
+                child: Text(
+                  'e',
+                  style: TextStyle(
+                    color: Color(0xFF800020),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
